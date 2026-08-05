f6541851062f8b3222f4abdbd19b21f442f7da12
apadilla-2024102 <apadilla-2024102@kinal.edu.gt>
2026-08-05 00:55:50 -0600

    auth: add RefreshTokenAsync and RevokeRefreshTokenAsync to IAuthService

--- auth-service/src/AuthService.Application/Interfaces/IAuthService.cs
+++ auth-service/src/AuthService.Application/Interfaces/IAuthService.cs
@@ -8,6 +8,8 @@ public interface IAuthService
 
     Task<RegisterResponseDto> RegisterAsync(RegisterDto registerDto);
     Task<AuthResponseDto> LoginAsync(LoginDto loginDto);
+    Task<AuthResponseDto> RefreshTokenAsync(string refreshToken);
+    Task<bool> RevokeRefreshTokenAsync(string refreshToken);
     Task<EmailResponseDto> VerifyEmailAsync(VerifyEmailDto verifyEmailDto);
     Task<EmailResponseDto> ResendVerificationEmailAsync(ResendVerificationDto resendDto);
     Task<EmailResponseDto> ForgotPasswordAsync(ForgotPasswordDto forgotPasswordDto);
